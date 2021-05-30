52d720ee1c8fa2caee9e56a374d88d4927bc360a
DEEP <deepsingh44@gmail.com>
2021-05-30 12:39:24 +0530

    Spring boot micro service integration

---
+++ DucatService/src/main/java/com/deepsingh44/demo/DucatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deepsingh44.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucatServiceApplication.class, args);
+	}
+
+}
